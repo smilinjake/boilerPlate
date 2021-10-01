2bb819f856d58cc68ebcdb18ce0fe5bc01435672
Jacob Ward <smilinjake@gmail.com>
2021-09-30 22:42:29 -0500

    starting the boilerplate

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.json());
+
+app.listen(port, (error) => {
+  if (error) console.log(error);
+  console.log(`Listening to port ${port}`);
+})
